9ddceebc72a12a71b16e807357c9ecfe1c2a1217
Suat Karakusoglu <suatkarakusoglu@gmail.com>
2018-06-13 23:56:36 +0300

    Initial Commit

---
+++ ZMLocalBroadcaster/ZMLocalBroadcaster.h
@@ -0,0 +1,19 @@
+//
+//  ZMLocalBroadcaster.h
+//  ZMLocalBroadcaster
+//
+//  Created by Suat Karakusoglu on 13.06.2018.
+//  Copyright © 2018 Suat Karakusoglu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZMLocalBroadcaster.
+FOUNDATION_EXPORT double ZMLocalBroadcasterVersionNumber;
+
+//! Project version string for ZMLocalBroadcaster.
+FOUNDATION_EXPORT const unsigned char ZMLocalBroadcasterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZMLocalBroadcaster/PublicHeader.h>
+
+
